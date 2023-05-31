4e0dae7eb5c3758e8d01c778be8b698bbeb037a0
Sumanthrr26 <Sumanthr26@gmail.com>
2023-05-31 22:48:14 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-<<<<<<< HEAD
+
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "../src/Components/Nav";
 import React from "react";
-function App() {
-  return <Navbar />;
-=======
 import "./App.css";
+import Navbar from "./Components/Nav";
+import React from "react";
+import "./App.css";
+
+  
+
+
 import AllRoutes from "./All Routes/AllRoutes";
 function App() {
+  
   return (
     <div className="App">
+      <Navbar />;
       <AllRoutes />
     </div>
   );
->>>>>>> f0590f0e1cd97e04bc583d096b5026208b9fde4a
+
 }
 export default App;
