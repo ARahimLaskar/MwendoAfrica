a2be1a213ca4b641e232205eb90c063d482ce083
Sumanthrr26 <Sumanthr26@gmail.com>
2023-05-31 22:54:20 +0530

    Navbar and footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,139 @@
-import React from "react";
+import { Box, Divider, Text, Link, HStack, Stack } from "@chakra-ui/react";
+import MwendoLogo from "../Images/logo.jpeg";
+import MwendoLogo2 from "../Images/logo2.png";
+function Footer({ links }) {
+  return (
+    <>
+      <Stack>
+        <HStack
+          flexDirection={["column", "row"]}
+          justifyContent={["center", "space-around"]}
+          bg="#FFFFFF"
+          py="90px"
+          width={window.innerWidth}
+          textColor="white"
+          mb="-20px"
+        >
+          <Box
+            flexDirection="column"
+            display="flex"
+            justifyContent="space-between"
+            alignItems={{ base: "center", lg: "start" }}
+            mb="1rem"
+            order={[2, 1]}
+          >
+            <Box display="flex" mb="1.5rem" mt="-60px">
+              <Link href="#" display="inline-flex" fontSize="2rem">
+                <Box
+                  as="img"
+                  w={50}
+                  h={50}
+                  mt="20px"
+                  src={MwendoLogo}
+                  alt="Koo"
+                />
+                <Box as="img" w={200} h={70} src={MwendoLogo2} alt="Koo" />
+              </Link>
+            </Box>
+          </Box>
 
-export default function Footer() {
-  return <div>Footer</div>;
+          <Box
+            display="flex"
+            textAlign="left"
+            order={[1, 2]}
+            mb={["20px", null]}
+            pl={"-90px"}
+          >
+            <Box ml={-210} mr={30}>
+              <Text
+                fontSize="18px"
+                fontWeight="bold"
+                mb={2}
+                color="rgb(24,20,116)"
+              >
+                Products
+              </Text>
+              <Divider mb={2} />
+              {links.quickLinks.map((link) => (
+                <Link href={link.href} key={link.text}>
+                  <Text fontSize="14px" color="rgb(208,211,220)">
+                    {link.text}
+                  </Text>
+                </Link>
+              ))}
+            </Box>
+            <Box mr={30}>
+              <Text
+                fontSize="18px"
+                fontWeight="bold"
+                mb={2}
+                color="rgb(24,20,116)"
+              >
+                Company
+              </Text>
+              <Divider mb={2} />
+              {links.companyLinks.map((link) => (
+                <Link href={link.href} key={link.text}>
+                  <Text fontSize="14px" color="rgb(208,211,220)">
+                    {link.text}
+                  </Text>
+                </Link>
+              ))}
+            </Box>
+            <Box mr={30}>
+              <Text
+                fontSize="18px"
+                fontWeight="bold"
+                mb={2}
+                color="rgb(24,20,116)"
+              >
+                Support
+              </Text>
+              <Divider mb={2} />
+              {links.supportLinks.map((link) => (
+                <Link href={link.href} key={link.text}>
+                  <Text fontSize="14px" color="rgb(208,211,220)">
+                    {link.text}
+                  </Text>
+                </Link>
+              ))}
+            </Box>
+            <Box mr={30}>
+              <Text
+                fontSize="18px"
+                fontWeight="bold"
+                mb={2}
+                color="rgb(24,20,116)"
+              >
+                Follow Us
+              </Text>
+              <Divider mb={2} />
+              {links.socialLinks.map((link) => (
+                <Link href={link.href} key={link.text}>
+                  <Text fontSize="14px" color="rgb(208,211,220)">
+                    {link.text}
+                  </Text>
+                </Link>
+              ))}
+            </Box>
+          </Box>
+        </HStack>
+
+        <HStack
+          pb="1rem"
+          justifyContent="space-around"
+          flexDirection={["column", "row"]}
+        >
+          <Box
+            flexDirection="column"
+            justifyContent="space-between"
+            textAlign={["center", "left"]}
+            py="0.5rem"
+          ></Box>
+        </HStack>
+      </Stack>
+    </>
+  );
 }
+
+export default Footer;
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -19,6 +19,66 @@ import axios from "axios";
 import ProductCard from "./ProductCard";
 
 export default function Home() {
+  const links = {
+    quickLinks: [
+      { text: "Features", href: "#" },
+      {
+        text: "Pricing",
+        href: "#",
+      },
+      { text: "Case Studies", href: "#" },
+      {
+        text: "Reviews",
+        href: "#",
+      },
+      { text: "Updates", href: "#" },
+    ],
+    supportLinks: [
+      { text: "Getting Started", href: "#" },
+      {
+        text: "Help Center",
+        href: "#",
+      },
+      {
+        text: "Server status",
+        href: "#",
+      },
+      {
+        text: "Report a bug",
+        href: "#",
+      },
+      {
+        text: "Chat Support",
+        href: "#",
+      },
+    ],
+    companyLinks: [
+      { text: "About", href: "#" },
+      {
+        text: "Contact Us",
+        href: "#",
+      },
+      { text: "Careers", href: "#" },
+      {
+        text: "Culture",
+        href: "#",
+      },
+      { text: "Blog", href: "#" },
+    ],
+    socialLinks: [
+      { text: "Facebook", href: "#" },
+      {
+        text: "Twitter",
+        href: "#",
+      },
+      { text: "Instagram", href: "#" },
+      {
+        text: "Linkedin",
+        href: "#",
+      },
+      { text: "Youtube", href: "#" },
+    ],
+  };
   const [carState, setCarState] = useState([]);
   const [busState, setBusState] = useState([]);
   const [truckState, setTruckState] = useState([]);
